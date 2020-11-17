93c78fd0c9c3dff309e66a39058357fb2919e432
Nitin Kumar <nitin@nitins-MacBook-Air.local>
2020-11-16 22:11:32 -0500

    blazor component assignment

---
+++ BlazorProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
